ca0dda1cc62496b41205a3bfaffee92b88a572fd
John Seong <wonmor@gmail.com>
2023-09-12 18:47:02 +0900

    Update App.js

--- App.js
+++ App.js
@@ -15,51 +15,97 @@ import {
   StyleSheet,
   Text,
   Linking,
-  FlatList,
-} from 'react-native';
+  TouchableOpacity,
+} from "react-native";
 
 function HomeScreen({ navigation }) {
-
   const data = [
-    'Foreword', 'Introduction', 'Getting the best from this book', 'Worrying',
-    'Looking on the bright side', 'Body language', 'Boundaries', 'Eye contact',
-    'Tone of voice', 'Dress sense', 'Distortions of the truth', 
-    'Misunderstandings other people might have about you', 'Conversation', 
-    'General knowledge', 'Names', 'Humour and conflict',
-    'Sexually related problems and points about going out', 'Nights Out',
-    'Chat ups', 'Invitation', 'Personal Security', 'Rape Crisis', 'Finding the right friends',
-    'Keeping a clean slate', 'Coming Clean', 'Education', 'Living Away from Home',
-    'Using the Phone', 'Guests', 'Jobs and Interviews', 'Driving', 'Travelling abroad',
-    'Bartering', 'Opportunities', 'A Personal in depth analysis of the problem', 'Further Reading'
+    "Foreword",
+    "Introduction",
+    "Getting the best from this book",
+    "Worrying",
+    "Looking on the bright side",
+    "Body language",
+    "Boundaries",
+    "Eye contact",
+    "Tone of voice",
+    "Dress sense",
+    "Distortions of the truth",
+    "Misunderstandings other people might have about you",
+    "Conversation",
+    "General knowledge",
+    "Names",
+    "Humour and conflict",
+    "Sexually related problems and points about going out",
+    "Nights Out",
+    "Chat ups",
+    "Invitation",
+    "Personal Security",
+    "Rape Crisis",
+    "Finding the right friends",
+    "Keeping a clean slate",
+    "Coming Clean",
+    "Education",
+    "Living Away from Home",
+    "Using the Phone",
+    "Guests",
+    "Jobs and Interviews",
+    "Driving",
+    "Travelling abroad",
+    "Bartering",
+    "Opportunities",
+    "A Personal in depth analysis of the problem",
+    "Further Reading",
   ];
 
   return (
     <ScrollView style={styles.container}>
-      <Text style={[styles.title, { fontFamily: "Quicksand_600SemiBold" }]}>Aspie Guide</Text>
+      <Text style={[styles.title, { fontFamily: "Quicksand_600SemiBold" }]}>
+        Aspie Guide
+      </Text>
 
-<Text style={[styles.header, { fontFamily: "Quicksand_600SemiBold" }]}>Contents</Text>
-
+      <Text style={[styles.header, { fontFamily: "Quicksand_600SemiBold" }]}>
+        Contents
+      </Text>
       {/* Table of Contents Grid */}
       <View style={styles.gridContainer}>
         {data.map((item, index) => (
-          <View key={index} style={styles.gridItem}>
-            <Text style={styles.contents}>{item}</Text>
-          </View>
+          <TouchableOpacity
+            key={index}
+            onPress={() => navigation.navigate(item)}
+          >
+            <View style={styles.gridItem}>
+              <Text
+                style={{
+                  ...styles.contents,
+                  fontFamily: "Quicksand_600SemiBold",
+                }}
+              >
+                {item}
+              </Text>
+            </View>
+          </TouchableOpacity>
         ))}
       </View>
 
       {/* Acknowledgments */}
       <Text style={styles.acknowledgment}>
-        The book, ‘Coping: A Survival Guide for People with Asperger Syndrome’ was written by Marc Segar. ...
-        (Continue with the rest of the acknowledgments as given before)
-        ...
-        Special thanks are due to Pauline Greenhough for her typing.
+        The book, ‘Coping: A Survival Guide for People with Asperger Syndrome’
+        was written by Marc Segar. Special thanks are due to Pauline
+        Greenhough for her typing.
       </Text>
 
       {/* App Development */}
       <Text style={styles.development}>
-        The app has been developed by John Wonmo Seong for better accessibility to the public. 
-        Contact: <Text style={styles.link} onPress={() => Linking.openURL('mailto:wonmos@uci.edu')}>wonmos@uci.edu</Text> at University of California, Irvine.
+        The app has been developed by John Wonmo Seong for better accessibility
+        to the public. Contact:{" "}
+        <Text
+          style={styles.link}
+          onPress={() => Linking.openURL("mailto:wonmos@uci.edu")}
+        >
+          wonmos@uci.edu
+        </Text>{" "}
+        at University of California, Irvine.
       </Text>
     </ScrollView>
   );
@@ -141,31 +187,55 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
   },
   title: {
-    fontSize: 24,
-    textAlign: 'center',
+    fontSize: 26,
+    textAlign: "center",
     marginBottom: 20,
+    color: "#333",
   },
   header: {
-    fontSize: 20,
-    marginTop: 10,
-    marginBottom: 10,
+    fontSize: 22,
+    marginTop: 15,
+    marginBottom: 15,
+    color: "#555",
+  },
+  gridContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  gridItem: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
   },
   contents: {
-    fontFamily: "Quicksand_400Regular",
-    marginBottom: 20,
+    fontFamily: "Quicksand_600SemiBold",
+    color: "#444",
+    fontSize: 16,
   },
   acknowledgment: {
-    fontFamily: "Quicksand_400Regular",
+    fontFamily: "Quicksand_600SemiBold",
     marginBottom: 20,
+    color: "#666",
   },
   development: {
-    fontFamily: "Quicksand_400Regular",
-    marginBottom: 20,
+    fontFamily: "Quicksand_600SemiBold",
+    marginBottom: 30,
+    color: "#666",
   },
   link: {
-    color: 'blue',
+    color: "#2196F3",
   },
 });
