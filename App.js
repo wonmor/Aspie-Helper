ba2c511533d5e8b2b27e29c751714c75bf274e25
John Seong <wonmor@gmail.com>
2023-09-12 18:27:02 +0900

    Updated button style

--- App.js
+++ App.js
@@ -10,14 +10,25 @@ import {
 import { Button, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { Text, TouchableOpacity } from "react-native";
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
+      <TouchableOpacity
+        style={{ backgroundColor: "#2196F3", padding: 10, borderRadius: 5 }}
         onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
-      />
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            color: "white",
+            fontFamily: "Quicksand_600SemiBold",
+          }}
+        >
+          Go to notifications
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -25,7 +36,20 @@ function HomeScreen({ navigation }) {
 function NotificationsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+      <TouchableOpacity
+        style={{ backgroundColor: "#2196F3", padding: 10, borderRadius: 5 }}
+        onPress={() => navigation.goBack()}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            color: "white",
+            fontFamily: "Quicksand_600SemiBold",
+          }}
+        >
+          Go back home
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -47,8 +71,34 @@ export default function App() {
     return (
       <NavigationContainer>
         <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+          <Drawer.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerTitleStyle: {
+                fontFamily: "Quicksand_600SemiBold",
+              },
+              drawerLabel: ({ focused, color }) => (
+                <Text style={{ fontFamily: "Quicksand_600SemiBold", color }}>
+                  Home
+                </Text>
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Notifications"
+            component={NotificationsScreen}
+            options={{
+              headerTitleStyle: {
+                fontFamily: "Quicksand_600SemiBold",
+              },
+              drawerLabel: ({ focused, color }) => (
+                <Text style={{ fontFamily: "Quicksand_600SemiBold", color }}>
+                  Notifications
+                </Text>
+              ),
+            }}
+          />
         </Drawer.Navigator>
       </NavigationContainer>
     );
