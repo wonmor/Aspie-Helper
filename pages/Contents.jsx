0040da6f0a2c62b9629132d141fcbeb6ece7fd73
John Seong <wonmor@gmail.com>
2023-09-14 16:42:26 +0900

    Added two more articles

--- pages/Contents.jsx
+++ pages/Contents.jsx
@@ -695,3 +695,120 @@ export function Boundaries() {
     </ScrollView>
   );
 }
+
+export function EyeContact() {
+  return (
+    <ScrollView style={styles.container}>
+      <Image
+        style={styles.imageStyle}
+        source={require("../assets/monkey1.jpg")}
+      />
+
+      {/* Photo credit */}
+      <Text style={styles.creditText}>Photo by John Seong</Text>
+
+      <Text style={styles.text}>
+        Eye contact is hard to get right because it is hard to tell whether you
+        are giving someone too much eye contact or too little when they are
+        talking to you.
+      </Text>
+
+      <Text style={styles.text}>
+        While people are not talking and when you are not talking to them, it is
+        often best not to look at them. This is because people can usually see
+        that you are looking at them out of the corner of their eyes and this
+        may make them feel uncomfortable, in which case they might talk about
+        you behind your back. To control your gaze might be difficult for you
+        but it is by no means impossible.
+      </Text>
+
+      <Text style={styles.text}>
+        If you point at someone when you are talking about them to someone else,
+        this may seem rude if they notice. If you are arguing with someone and
+        point at them while giving eye contact, this may come across as quite
+        aggressive. Try not to point at people - it will help you stay out of
+        trouble.
+      </Text>
+
+      <Text style={styles.text}>
+        When you are talking to someone or they are talking to you, you are
+        expected to look at them bearing in mind the following guidelines:
+      </Text>
+
+      <Text style={styles.text}>
+        o To look at someone for less than one third of the time may be
+        communicating that either you are shy (if you keep looking down) or you
+        are dishonest (if you keep looking to the side).
+      </Text>
+
+      <Text style={styles.text}>
+        o To look at someone for more than two thirds of the time may be
+        communicating that either you like them (if you are looking at the face
+        as a whole) or you are aggressive (if you are looking straight into
+        their eyes)
+      </Text>
+
+      <Text style={styles.text}>
+        o To look at someone for the whole time giving steady and unbroken eye
+        contact can mean one of two things. Either you are challenging them (the
+        aggressive gaze) or you fancy them (the intimate gaze). However in other
+        cultures (e.g. Mediterranean Europe) it can also symbolise
+        companionship. For someone with autism it can be very difficult because
+        first we have to be sure that it IS appropriate. Also fixed eye contact
+        can forcefully distract us when we try to talk.
+      </Text>
+
+      {/* Bottom Spacer */}
+      <View style={{ paddingBottom: 40 }} />
+    </ScrollView>
+  );
+}
+
+export function ToneOfVoice() {
+    return (
+        <ScrollView style={styles.container}>
+            <Image
+        style={styles.imageStyle}
+        source={require("../assets/monkey2.jpg")}
+      />
+
+      {/* Photo credit */}
+      <Text style={styles.creditText}>Photo by John Seong</Text>
+
+            <Text style={styles.text}>
+                You might be one of these people who almost talks in a single tone without knowing it.
+            </Text>
+
+            <Text style={styles.text}>
+                Ask a trustworthy person if this is true and if it is you may have to exaggerate the intonation in your voice to emphasise what you say, but not too much. This will sound artificial at first.
+            </Text>
+
+            <Text style={styles.text}>
+                If you are reading a story-book to a child then the more intonation the better.
+            </Text>
+
+            <Text style={styles.text}>
+                The intonation in our voices is extremely important in determining whether we are being enthusiastic or sarcastic about something. It is also important in telling whether we mean something seriously or just as a joke.
+            </Text>
+
+            <Text style={styles.text}>
+                To talk in a single tone can make it sound as if you’re depressed. When talking about something good or exciting you have to make yourself sound excited too, otherwise people tend to think it sounds strange.
+            </Text>
+
+            <Text style={styles.text}>
+                If you are a young man whose voice is breaking, then if you find it more comfortable just let it break for good. It may sound strange at first on the inside but it will be sounding much more natural on the outside. If you are worried about what your friends might think which should only be a short term problem anyway, it may be useful to take the opportunity of letting your voice break while you are changing schools.
+            </Text>
+
+            <Text style={styles.text}>
+                Finally, remember not to speak too loudly and not to speak too quietly. This should depend on the distance between you and the other person and the voice should be quieter when a bit of secrecy is needed. Whisper when everyone else is whispering (or when there is someone asleep nearby).
+            </Text>
+
+            <Text style={styles.text}>
+                At times when you may need to talk extra loudly and clearly (e.g. on stage or in a play) then you may want to project your voice. To do this keep a nice straight relaxed posture and imagine that your voice is coming from your stomach, however strange this may seem.
+            </Text>
+
+            {/* Bottom Spacer */}
+            <View style={{ paddingBottom: 40 }} />
+        </ScrollView>
+    );
+}
