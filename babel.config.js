7950c2f11284f8b6463ea6f33c75a6c570008d98
John Seong <wonmor@gmail.com>
2023-09-28 06:55:42 -0700

    Fixed Tailwind CSS Issue

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            "@breathly": "./src",
+          },
+        },
+      ],
+      "nativewind/babel",
+    ],
   };
 };
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    printWidth: 100,
+    importOrder: ["<THIRD_PARTY_MODULES>", "^@breathly/(.*)$", "^[./]"],
+    plugins: ["@trivago/prettier-plugin-sort-imports"],
+  };
+  
